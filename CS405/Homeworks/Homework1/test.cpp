f5d943a0eaf71161808d8d1046cb1522b589a14c
Aven <aven@aven-M11x-R2.(none)>
2014-02-06 03:58:18 -0900

    Fixed test.cpp to work with floats

--- CS405/Homeworks/Homework1/test.cpp
+++ CS405/Homeworks/Homework1/test.cpp
@@ -12,7 +12,7 @@ int main(){
 
 	NeuralNet n1(s, 4);	//Create NeuralNet with 4 layers of the given sizes
 
-	double in[32];			//Array to store input for evaluate function
+	float in[32];			//Array to store input for evaluate function
 	for(int i=0; i<32; i++)		//Make each input 1 for the test
 		in[i]=100;
 	
@@ -20,8 +20,6 @@ int main(){
 	auto start = std::chrono::steady_clock::now();
 	auto curr = start;
 	int count = 0;
-	
-	std::cout<< *(n1.evaluate(in))<<"\n";
 
 	//See how many evaluations can be performed in 1 second (do 10 seconds then divide)
 	do{
@@ -29,15 +27,7 @@ int main(){
 		count++;
 		curr = std::chrono::steady_clock::now();
 	}
-	while((std::chrono::duration<double>(curr-start)).count() < 10.2);	//.2 found to be time for empty loop
+	while((std::chrono::duration<float>(curr-start)).count() < 10.2);	//.2 found to be time for empty loop
 
-	std::cout << count/10 << "\n";
-
-	/* OLD CODE FOR FINDING EMPTY LOOP TIME
-	for(int i=0; i<44000; i++){
-		count++;
-		curr = std::chrono::steady_clock::now();
-		std::chrono::duration<double>(curr-start);
-	}
-	std::cout << std::chrono::duration<double>(curr-start).count() << "\n";*/	
+	std::cout << "Evaluations per second: " << count/10 << "\n";
 }
